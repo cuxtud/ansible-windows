cc9bd0830194e112b0df4e7885bb4eac963c634e
Anish Abraham <vjanish1984@gmail.com>
2019-03-28 13:31:17 +0000

    updated playbook to use become

--- files/ntpcheck.ps1
+++ files/ntpcheck.ps1
@@ -1,4 +1,5 @@
 # NTP Service Check Local Server
+Start-transcript -path C:\Temp\scriptsummary.txt
 $NtpSrvStat = (Get-Service -Name W32Time).Status
 IF ($NtpSrvStat -eq "Stopped")
 {
@@ -6,3 +7,4 @@ Cmd /c net start w32time
 w32tm /resync
 Net Time /Set /Y
 }
+Stop-Transcript
